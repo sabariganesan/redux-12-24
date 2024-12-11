e4a6f48420eb40fca94a9f8919301c8d18cbf2e5
Rajeshdev90 <rajesh>
2024-12-11 13:27:52 +0530

    data list redux action implementation

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import countReducer from "../reducer/counterReducer";
 import counterReducerSlice from "../reducer/counterReducerSlice";
+import dataListReducer from "../reducer/dataListReducer";
 
 const store = configureStore({
   reducer: {
     auth: countReducer,
     slice: counterReducerSlice,
+    list: dataListReducer,
   },
 });
 
